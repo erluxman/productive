bd975bab27987837ee97948ea7920a63e9c68f2f
erluxman <erluxman@gmail.com>
2020-06-27 07:19:52 +0545

    Added code coverage for Appbar

--- test/features/home/home_screen_test.dart
+++ test/features/home/home_screen_test.dart
@@ -1,20 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:productive/common/resources/r.dart';
+import 'package:productive/features/home/widgets/home_app_bar.dart';
+import 'package:productive/features/home/widgets/home_bottom_nav_bar.dart';
 import 'package:productive/features/home/widgets/home_fab.dart';
 import 'package:productive/features/home/widgets/home_screen.dart';
+
 import '../../utils/widget/test_utils.dart';
 
 void main() {
   group("All required widgets are on the Home screen", () {
     testWidgets(
-      "Fab with Text + Add Todo exist",
+      "Fab,BottomNav and AppbarContent exist",
       (WidgetTester tester) async {
-        await  tester.pumpWidget(const HomeScreen().asScaffold);
+        await tester.pumpWidget(const HomeScreen().asScaffold);
         expect(find.byType(Fab), findsOneWidget);
         expect(find.byType(RaisedButton), findsOneWidget);
+        expect(find.byType(BottomNav), findsOneWidget);
+        expect(find.byType(AppbarContent), findsOneWidget);
+
         await tester.pumpWidget(const Fab().testWidget);
-        expect(find.text(R.string.addTodo ), findsOneWidget);
+        expect(find.text(R.string.addTodo), findsOneWidget);
       },
     );
   });
