2af773fcc1c2db6d07041044f4ae7a951027e637
erluxman <erluxman@gmail.com>
2020-07-19 10:29:28 +0545

    Removed some flaky tests

--- test/playgorund/testmain_test.dart
+++ test/playgorund/testmain_test.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:productive/common/resources/r.dart';
-import 'package:productive/features/home/widgets/home_screen.dart';
 import 'package:productive/playground/test_main.dart';
-
-import '../test_utils/test_utils.dart';
 
 void main() {
   group("Dummy widget test", () {
@@ -42,36 +37,6 @@ void main() {
         await tester.pumpAndSettle();
         expect(find.byKey(const Key("gd1")), findsOneWidget);
         expect(find.text("Jump to screen2"), findsOneWidget);
-      },
-    );
-  });
-
-  group("dummy tests for reference doesn't mean anything at all", () {
-    testWidgets(
-      "When a button is long pressed, it has 80% size",
-      (WidgetTester tester) async {
-        await tester.pumpWidget(const HomeScreen().asScaffold);
-        await tester.press(find.byWidgetPredicate((widget) =>
-            (widget is SvgPicture) &&
-            (widget.pictureProvider as ExactAssetPicture).assetName ==
-                R.svg.homeIcon));
-        await tester.pump();
-        SvgPicture homeIcon = tester.firstWidget(find.byWidgetPredicate(
-            (widget) =>
-                (widget is SvgPicture) &&
-                (widget.pictureProvider as ExactAssetPicture).assetName ==
-                    R.svg.homeIcon));
-
-        await tester.press(find.byWidgetPredicate((widget) =>
-            (widget is SvgPicture) &&
-            (widget.pictureProvider as ExactAssetPicture).assetName ==
-                R.svg.statsIcon));
-        await tester.pump();
-        homeIcon = tester.firstWidget(find.byWidgetPredicate((widget) =>
-            (widget is SvgPicture) &&
-            (widget.pictureProvider as ExactAssetPicture).assetName ==
-                R.svg.homeIcon));
-        homeIcon.asScaffold;
       },
     );
   });
