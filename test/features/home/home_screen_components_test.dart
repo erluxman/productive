9c0585a2a59aedc4a5ef979b9f9131180668d4c9
erluxman <erluxman@gmail.com>
2020-07-30 09:39:00 +0545

    Some more tests

--- test/features/home/home_screen_components_test.dart
+++ test/features/home/home_screen_components_test.dart
@@ -1,7 +1,10 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:productive/common/resources/r.dart';
 import 'package:productive/features/home/widgets/animated_circle_progress.dart';
+import 'package:productive/features/home/widgets/animated_nav_button.dart';
+
+import '../../test_utils/test_utils.dart';
 
 void main() {
   group("Test Animated Motivation Meter", () {
@@ -21,5 +24,64 @@ void main() {
     });
   });
 
+  group("Test Animated Nav Button", () {
+    setUpAll(() {
+      R.buildMode.isTesting = true;
+    });
 
+    tearDownAll(() {
+      R.buildMode.isTesting = false;
+    });
+
+    testWidgets("gestures are working", (WidgetTester tester) async {
+      // ignore: prefer_const_constructors
+      NavButtonsState state = NavButtonsState();
+      void select(int index) {
+        state = state.copyWith(selectedIndex: 1);
+      }
+
+      void touch(int index) {
+        state = state.copyWith(pressedIndex: 1);
+      }
+
+      void unTouch() {
+        state = state.copyWith(pressedIndex: -1);
+      }
+
+      final navBar = Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          AnimatedNavIcon(
+            navState: state,
+            select: () => select(0),
+            unTouch: () => unTouch,
+            touch: () => touch(0),
+            child: NavIcon(
+              position: 0,
+              svgPath: R.svg.homeIcon,
+              navState: state,
+            ),
+          ),
+          AnimatedNavIcon(
+            navState: state,
+            select: () => select(1),
+            unTouch: () => unTouch,
+            touch: () => touch(1),
+            child: NavIcon(
+              position: 1,
+              svgPath: R.svg.statsIcon,
+              navState: state,
+            ),
+          ),
+        ],
+      );
+      await tester.pumpWidget(navBar.asScaffold);
+      expect(find.byType(GestureDetector), findsNWidgets(2));
+
+//      await tester.press(find.byType(GestureDetector));
+//      await tester.pumpAndSettle();
+//      await tester.longPress(find.byType(GestureDetector));
+//      await tester.pumpAndSettle();
+    });
+  });
 }
